d6734fd19fea2aab0b170c18620f0acd7b61266c
langat justus <justuslangat78@gmail.com>
2020-09-29 12:47:47 +0300

    fixed the structure

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
 from app import app, db 
 
-@app.route('/', methods=['GET', 'POST'])        
+@app.route('/', methods=['GET', 'POST'])    
+@app.route('/index', methods=['GET', 'POST'])    
