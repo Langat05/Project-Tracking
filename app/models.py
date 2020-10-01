b8407f7729da83e287e455d15020903933a5969e
luqqy <you@example.com>
2020-10-01 09:39:42 +0300

    updated auth folder

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from time import time
-import jwt
+import jw
 from app import app
 
 
