dcd6c056dad0c09cb0ace05d303d071e29a20c19
luqqy <you@example.com>
2020-10-01 07:42:06 +0300

    updated index.html with new functionalities

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db 
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime 
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True) 
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+   
+
